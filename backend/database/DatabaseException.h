adacc505b501924b95001b18f8d70c64b25bb952
Oleh Klepatskyi <o.klepatskyi@gmail.com>
2022-12-05 00:09:04 +0200

    String buffer in makeErrorMsg

--- backend/database/DatabaseException.h
+++ backend/database/DatabaseException.h
@@ -2,13 +2,16 @@
 #define DATABASEEXCEPTION_H
 #include <QSqlError>
 #include <stdexcept>
+#include <QDebug>
+#include <sstream>
 
-inline const char* makeErrorMsg(const QString& query, const QSqlError& error, const char* src);
+inline std::string makeErrorMsg(const QString& query, const QSqlError& error, const char* src);
 
 class DatabaseException : public std::runtime_error{
 public:
-    DatabaseException() : std::runtime_error("") {};
-    DatabaseException(const char* msg) : std::runtime_error(msg) {}
+    DatabaseException(const std::string &msg) : std::runtime_error(msg) {
+        qDebug() << "DatabaseException created with message:" << msg.c_str();
+    }
 };
 
 class SqlQueryException : public DatabaseException {
@@ -17,15 +20,15 @@ public:
         DatabaseException(makeErrorMsg(query, error, src)) {}
 };
 
-// todo replace with string buffer
-inline const char* makeErrorMsg(const QString& query, const QSqlError& error, const char* src)
+inline std::string makeErrorMsg(const QString& query, const QSqlError& error, const char* src)
 {
-    return (QString(src) +
-            QString(": ") +
-            QString("while executing query '") +
-            query +
-            QString("'\n\t") +
-            error.text()).toLatin1();
+    std::stringstream sstream;
+    sstream << src << ": " <<
+               "while executing query '" <<
+               query.toLatin1().toStdString() <<
+               "'\n\t" <<
+               error.text().toLatin1().toStdString();
+    return sstream.str();
 }
 
 #endif // DATABASEEXCEPTION_H
